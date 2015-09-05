8c05354cf68d0b704b4d4f1be90405bea89e828a
shafiq-mohammed <shafiqdms1@gmail.com>
2015-09-05 15:41:20 -0500

    Template file

---
+++ app.js
@@ -0,0 +1,7 @@
+/*
+@author Shafiq Mohammed
+@@date 09-02-2015
+In order to learn how NodeJS handles multiple requests, let us create a 
+restaurant mockup that only consists of one item (aka querying the database
+for data) with multiple customers.
+*/
